3f6f69cc6dbef8aa2921e8a073047b4b3f600133
Gary Tan <saba555279@gmail.com>
2021-05-12 01:57:29 +0800

    add color log

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install homebrew & Xcode Command Line Tools
+echo -e '\033[92m''[*] Installing homebrew...''\033[0m'
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Apple Silicon add PATH
@@ -12,6 +13,7 @@ fi
 brew analytics off
 
 # brew install
+echo -e '\033[92m''[*] Installing brew package...''\033[0m'
 brew install \
 nmap \
 git \
@@ -20,11 +22,13 @@ mas \
 java11
 
 # fish config
+echo -e '\033[92m''[*] Move fish config to ''\033[93m''~/.config/fish/''\033[92m''...''\033[0m'
 if [ -d "~/.config/fish/" ]; then
   cp -R ./config/fish/ ~/.config/fish/
 fi
 
 # brew cask install
+echo -e '\033[92m''[*] Installing homebrew/cask package...''\033[0m'
 brew install --cask \
 iterm2 \
 spotify \
@@ -44,11 +48,13 @@ iina
 # microsoft-remote-desktop \
 
 # brew cask/versions install
+echo -e '\033[92m''[*] Installing homebrew/cask-versions package...''\033[0m'
 brew tap homebrew/cask-versions
 brew install --cask \
 firefox-nightly
 
 # brew cask/font install
+# echo -e '\033[92m''[*] Installing homebrew/cask-fonts package...''\033[0m'
 # brew tap homebrew/cask-fonts
 # brew install \
 # font-noto-sans-cjk-tc \
@@ -60,9 +66,9 @@ firefox-nightly
 # 1105332179|KKBOX
 # 975937182 |Fantastical
 # 539883307 |LINE
-# 1176895641|Spark
+# 1176895641|Spark 
 # 1451685025|WireGuard
-
+echo -e '\033[92m''[*] Installing App Store applications...''\033[0m'
 mas install \
 1451685025 \
 539883307 \
